a4ee3238aad9d20e86e10e72e01ed017bc6659d8
jiftechnify <jiftech.stlfy@gmail.com>
2021-05-02 17:19:50 +0900

    Refactor: efficient bitmap format(?)

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -59,41 +59,42 @@ const PixelColor LG = {187, 187, 187};
 const PixelColor WH = {255, 255, 255};
 const int AEGIS_WIDTH = 20;
 const int AEGIS_HEIGHT = 34;
-const PixelColor AEGIS_BMP[AEGIS_WIDTH * AEGIS_HEIGHT] = {
-  WH, WH, WH, WH, WH, WH, WH, BK, BK, BK, BK, BK, BK, WH, WH, WH, WH, WH, WH, WH,
-  WH, WH, WH, WH, WH, BK, BK, DG, DG, DG, DG, DG, DG, BK, BK, WH, WH, WH, WH, WH,
-  WH, WH, WH, WH, BK, DG, DG, LG, LG, LG, LG, LG, LG, DG, DG, BK, WH, WH, WH, WH,
-  WH, WH, WH, BK, DG, LG, LG, LG, LG, LG, LG, LG, LG, LG, LG, DG, BK, WH, WH, WH,
-  WH, WH, BK, DG, LG, LG, LG, LG, LG, LG, LG, LG, LG, LG, LG, LG, DG, BK, WH, WH,
-  WH, WH, BK, DG, LG, LG, LG, LG, LG, LG, LG, LG, LG, LG, LG, LG, DG, BK, WH, WH,
-  WH, BK, DG, LG, LG, WH, WH, LG, LG, LG, LG, LG, LG, LG, LG, LG, LG, DG, BK, WH,
-  WH, BK, DG, LG, LG, BK, BK, BK, WH, LG, LG, WH, BK, BK, BK, LG, LG, DG, BK, WH,
-  BK, DG, DG, LG, BK, WH, BK, BK, WH, WH, LG, WH, BK, BK, WH, BK, LG, DG, DG, BK,
-  BK, DG, DG, LG, WH, WH, WH, BK, WH, WH, WH, WH, WH, BK, WH, WH, LG, DG, DG, BK,
-  BK, DG, DG, LG, WH, WH, BK, BK, WH, WH, WH, WH, BK, BK, WH, WH, LG, DG, DG, BK,
-  WH, BK, DG, LG, WH, WH, WH, WH, WH, BK, WH, WH, WH, WH, WH, WH, LG, DG, BK, WH,
-  WH, WH, BK, LG, BK, WH, WH, WH, WH, WH, WH, WH, WH, WH, WH, BK, LG, BK, WH, WH,
-  WH, WH, WH, BK, LG, BK, WH, WH, WH, BK, BK, WH, WH, WH, BK, LG, BK, WH, WH, WH,
-  WH, WH, WH, WH, BK, BK, BK, BK, BK, WH, WH, BK, BK, BK, BK, BK, WH, WH, WH, WH,
-  WH, WH, WH, WH, BK, WH, BK, BK, BK, BK, BK, BK, BK, BK, BK, WH, WH, WH, WH, WH,
-  WH, WH, WH, WH, WH, BK, BK, BK, BK, BK, BK, BK, BK, WH, WH, BK, WH, WH, WH, WH,
-  WH, WH, WH, WH, WH, WH, WH, BK, BK, BK, BK, BK, BK, BK, WH, WH, BK, BK, WH, WH,
-  WH, WH, WH, WH, WH, WH, WH, BK, BK, BK, BK, BK, BK, WH, BK, WH, WH, WH, BK, WH,
-  WH, WH, WH, WH, WH, WH, BK, BK, BK, BK, BK, BK, BK, BK, WH, BK, WH, WH, BK, WH,
-  WH, WH, WH, WH, WH, BK, BK, BK, BK, BK, BK, BK, BK, BK, BK, WH, BK, BK, WH, WH,
-  WH, WH, WH, WH, BK, BK, BK, BK, BK, BK, BK, BK, BK, BK, BK, BK, WH, WH, WH, WH,
-  WH, WH, WH, BK, BK, BK, BK, BK, BK, BK, BK, BK, BK, BK, BK, BK, BK, WH, WH, WH,
-  WH, WH, BK, WH, BK, BK, BK, BK, BK, BK, BK, BK, BK, BK, BK, BK, WH, BK, WH, WH,
-  WH, WH, WH, BK, WH, WH, BK, BK, BK, BK, BK, BK, BK, BK, WH, WH, BK, WH, WH, WH,
-  WH, WH, WH, WH, BK, BK, WH, WH, WH, WH, WH, WH, WH, WH, BK, BK, WH, WH, WH, WH,
-  WH, WH, WH, WH, WH, WH, BK, BK, BK, BK, BK, BK, BK, BK, WH, WH, WH, WH, WH, WH,
-  WH, WH, WH, WH, WH, WH, BK, WH, WH, BK, DG, WH, WH, BK, WH, WH, WH, WH, WH, WH,
-  WH, WH, WH, WH, WH, WH, BK, DG, DG, BK, DG, DG, WH, BK, WH, WH, WH, WH, WH, WH,
-  WH, WH, WH, WH, WH, WH, BK, DG, DG, BK, BK, DG, DG, BK, WH, WH, WH, WH, WH, WH,
-  WH, WH, WH, WH, WH, WH, BK, DG, DG, BK, WH, BK, BK, WH, WH, WH, WH, WH, WH, WH,
-  WH, WH, WH, WH, WH, BK, DG, DG, DG, BK, WH, WH, WH, WH, WH, WH, WH, WH, WH, WH,
-  WH, WH, WH, WH, WH, BK, WH, WH, WH, BK, WH, WH, WH, WH, WH, WH, WH, WH, WH, WH,
-  WH, WH, WH, WH, WH, WH, BK, BK, BK, WH, WH, WH, WH, WH, WH, WH, WH, WH, WH, WH,
+const int AEGIS_BMP_DATA_SIZE = (AEGIS_WIDTH * AEGIS_HEIGHT + 3) / 4;
+const unsigned char AEGIS_BMP_DATA[AEGIS_BMP_DATA_SIZE] = {
+  0xff, 0xfc, 0x00, 0x3f, 0xff,
+  0xff, 0xc1, 0x55, 0x43, 0xff,
+  0xff, 0x16, 0xaa, 0x94, 0xff,
+  0xfc, 0x6a, 0xaa, 0xa9, 0x3f,
+  0xf1, 0xaa, 0xaa, 0xaa, 0x4f,
+  0xf1, 0xaa, 0xaa, 0xaa, 0x4f,
+  0xc6, 0xbe, 0xaa, 0xaa, 0x93,
+  0xc6, 0x80, 0xeb, 0x02, 0x93,
+  0x16, 0x30, 0xfb, 0x0c, 0x94,
+  0x16, 0xfc, 0xff, 0xcf, 0x94,
+  0x16, 0xf0, 0xff, 0x0f, 0x94,
+  0xc6, 0xff, 0xcf, 0xff, 0x93,
+  0xf2, 0x3f, 0xff, 0xfc, 0x8f,
+  0xfc, 0x8f, 0xc3, 0xf2, 0x3f,
+  0xff, 0x00, 0x3c, 0x00, 0xff,
+  0xff, 0x30, 0x00, 0x03, 0xff,
+  0xff, 0xc0, 0x00, 0x3c, 0xff,
+  0xff, 0xfc, 0x00, 0x0f, 0x0f,
+  0xff, 0xfc, 0x00, 0x33, 0xf3,
+  0xff, 0xf0, 0x00, 0x0c, 0xf3,
+  0xff, 0xc0, 0x00, 0x03, 0x0f,
+  0xff, 0x00, 0x00, 0x00, 0xff,
+  0xfc, 0x00, 0x00, 0x00, 0x3f,
+  0xf3, 0x00, 0x00, 0x00, 0xcf,
+  0xfc, 0xf0, 0x00, 0x0f, 0x3f,
+  0xff, 0x0f, 0xff, 0xf0, 0xff,
+  0xff, 0xf0, 0x00, 0x0f, 0xff,
+  0xff, 0xf3, 0xc7, 0xcf, 0xff,
+  0xff, 0xf1, 0x45, 0xcf, 0xff,
+  0xff, 0xf1, 0x41, 0x4f, 0xff,
+  0xff, 0xf1, 0x4c, 0x3f, 0xff,
+  0xff, 0xc5, 0x4f, 0xff, 0xff,
+  0xff, 0xcf, 0xcf, 0xff, 0xff,
+  0xff, 0xf0, 0x3f, 0xff, 0xff,
 };
 
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
@@ -105,6 +106,32 @@ void WriteScaledPixel(PixelWriter* writer, int scale, int x, int y, const PixelC
       writer->Write(x * scale + px, y * scale + py, c);
     }
   }
+}
+
+const PixelColor* ColorFromColorNum2bit(unsigned char colNum) {
+  switch (colNum) {
+    case 0b00:
+      return &BK;
+      break;
+    case 0b01:
+      return &DG;
+      break;
+    case 0b10:
+      return &LG;
+      break;
+    case 0b11:
+      return &WH;
+      break;
+    default:
+      return &BK;
+  }
+}
+
+unsigned char ColorNum2bitAt(const unsigned char* bmp_data, int x, int y) {
+  int i = x + y * AEGIS_WIDTH;
+  int data_idx = i / 4;
+  int px_idx = i % 4;
+  return (bmp_data[data_idx] >> ((3 - px_idx) * 2)) & 0b11;
 }
 
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
@@ -126,7 +153,8 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   }
   for (int x = 0; x < AEGIS_WIDTH; ++x) {
     for (int y = 0; y < AEGIS_HEIGHT; ++y) {
-      WriteScaledPixel(pixel_writer, 4, x, y, AEGIS_BMP[x + y * AEGIS_WIDTH]);
+      const PixelColor* c = ColorFromColorNum2bit(ColorNum2bitAt(AEGIS_BMP_DATA, x, y));
+      WriteScaledPixel(pixel_writer, 4, x, y, *c);
     }
   }
   while (1) __asm__("hlt");
