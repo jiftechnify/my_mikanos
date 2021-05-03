c412096116b1c83fa68551f90e7d2d72c26346c7
jiftechnify <jiftech.stlfy@gmail.com>
2021-05-03 20:21:32 +0900

    Add: USB mouse input (by polling)

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -6,6 +6,13 @@
 #include "font.hpp"
 #include "console.hpp"
 #include "pci.hpp"
+#include "logger.hpp"
+#include "mouse.hpp"
+#include "usb/memory.hpp"
+#include "usb/device.hpp"
+#include "usb/classdriver/mouse.hpp"
+#include "usb/xhci/xhci.hpp"
+#include "usb/xhci/trb.hpp"
 
 const PixelColor BK{0, 0, 0};
 const PixelColor DG{102, 102, 102};
@@ -85,34 +92,6 @@ uint8_t ColorNum2bitAt(const uint8_t* bmp_data, int x, int y) {
   return (bmp_data[data_idx] >> ((3 - px_idx) * 2)) & 0b11;
 }
 
-const int kMouseCursorWidth = 15;
-const int kMouseCursorHeight = 24;
-const char mouse_cursor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
-  "@              ",
-  "@@             ",
-  "@.@            ",
-  "@..@           ",
-  "@...@          ",
-  "@....@         ",
-  "@.....@        ",
-  "@......@       ",
-  "@.......@      ",
-  "@........@     ",
-  "@.........@    ",
-  "@..........@   ",
-  "@...........@  ",
-  "@............@ ",
-  "@......@@@@@@@@",
-  "@......@       ",
-  "@....@@.@      ",
-  "@...@ @.@      ",
-  "@..@   @.@     ",
-  "@.@    @.@     ",
-  "@@      @.@    ",
-  "@       @.@    ",
-  "         @.@   ",
-  "         @@@   ",
-};
 
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
@@ -136,6 +115,35 @@ int printk(const char* format, ...) {
 const PixelColor kDesktopBGColor{45, 118, 237};
 const PixelColor kDesktopFGColor{255, 255, 255};
 
+// Intel製のUSB HCIのモードをEHCIモード(USB2.0)からxHCIモード(USB3.x)に切り替え
+void SwitchEhci2Xhci(const pci::Device& xhc_dev) {
+  bool intel_ehc_exist = false;
+  for (int i = 0; i < pci::num_device; ++i) {
+    if (pci::devices[i].class_code.Match(0x0cu, 0x03u, 0x20u) /* EHCI */ &&
+          0x8086 == pci::ReadVendorId(pci::devices[i])) {
+      intel_ehc_exist = true;
+      break;
+    }
+  }
+  if (!intel_ehc_exist) {
+    return;
+  }
+
+  uint32_t superspeed_ports = pci::ReadConfReg(xhc_dev, 0xdc);  // USB3PRM
+  pci::WriteConfReg(xhc_dev, 0xd8, superspeed_ports);  // USB3_PSSEN
+  uint32_t ehci2xhci_ports = pci::ReadConfReg(xhc_dev, 0xd4); // XUSB2PRM
+  pci::WriteConfReg(xhc_dev, 0xd0, ehci2xhci_ports);  // XUSB2PR
+  Log(kDebug, "SwitchEhci2Xhci: SS = %02x, xHCI = %02x\n",
+      superspeed_ports, ehci2xhci_ports);
+}
+
+char mouse_cursor_buf[sizeof(MouseCursor)];
+MouseCursor* mouse_cursor;
+
+void MouseObserver(int8_t displacement_x, int8_t displacement_y) {
+  mouse_cursor->MoveRelative({displacement_x, displacement_y});
+}
+
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   switch (frame_buffer_config.pixel_format) {
     case kPixelRGBResv8BitPerColor:
@@ -158,16 +166,12 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   DrawRectangle(*pixel_writer, {10, kFrameHeight - 40}, {30, 30}, {160, 160, 160});
 
   printk("Welcome to Mikan OS!\n");
+  SetLogLevel(kWarn);
 
-  for (int dy = 0; dy < kMouseCursorHeight; ++dy) {
-    for (int dx = 0; dx < kMouseCursorWidth; ++dx) {
-      if (mouse_cursor_shape[dy][dx] == '@') {
-        pixel_writer->Write(200 + dx, 100 + dy, BK);
-      } else if (mouse_cursor_shape[dy][dx] == '.') {
-        pixel_writer->Write(200 + dx, 100 + dy, WH);
-      }
-    }
-  }
+  mouse_cursor = new(mouse_cursor_buf) MouseCursor{
+    pixel_writer, kDesktopBGColor, {300, 200}
+  };
+
   for (int px = 0; px < AEGIS_WIDTH; ++px) {
     for (int py = 0; py < AEGIS_HEIGHT; ++py) {
       const PixelColor* c = ColorFromColorNum2bit(ColorNum2bitAt(AEGIS_BMP_DATA, px, py));
@@ -179,6 +183,7 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   auto err = pci::ScanAllBus();
   printk("ScanAllBus: %s\n", err.Name());
 
+  // PCIデバイス列挙
   for (int i = 0; i < pci::num_device; ++i) {
     const auto& dev = pci::devices[i];
     auto vendor_id = pci::ReadVendorId(dev.bus, dev.device, dev.function);
@@ -188,6 +193,73 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
         vendor_id, cc.base, cc.sub, cc.interface, dev.header_type);
   }
 
+  // xHCを探す
+  pci::Device* xhc_dev = nullptr;
+  for (int i = 0; i < pci::num_device; ++i) {
+    // base      = 0x0c: シリアルバスコントローラ
+    // sub       = 0x03: USBコントローラ
+    // interface = 0x30: xHCI
+    if (pci::devices[i].class_code.Match(0x0cu, 0x03u, 0x30u)) {
+      xhc_dev = &pci::devices[i];
+      
+      // Vendor ID = 0x8086: Intel製
+      if (0x8086 == pci::ReadVendorId(*xhc_dev)) {
+        break;
+      }
+    }
+  }
+  if (xhc_dev) {
+    Log(kInfo, "xHC has been found: %d.%d.%d\n",
+        xhc_dev->bus, xhc_dev->device, xhc_dev->function);
+  }
+
+  // xHCを制御するレジスタのMMIOアドレスを読み出す
+  // アドレスは BAR0 に書いてある
+  const WithError<uint64_t> xhc_bar = pci::ReadBar(*xhc_dev, 0);
+  Log(kDebug, "ReadBar: %s\n", xhc_bar.error.Name());
+  const uint64_t xhc_mmio_base = xhc_bar.value & ~static_cast<uint64_t>(0xf); // 64ビットのうち下位4ビットをマスク
+  Log(kDebug, "xHC mmio_base: %08lx\n", xhc_mmio_base);
+  
+  // xHCの初期化と起動
+  usb::xhci::Controller xhc{xhc_mmio_base};
+
+  if (0x8086 == pci::ReadVendorId(*xhc_dev)) {
+    SwitchEhci2Xhci(*xhc_dev);
+  }
+  {
+    auto err = xhc.Initialize();
+    Log(kDebug, "xhc.Initialize: %s\n", err.Name());
+  }
+  Log (kInfo, "xHC starting\n");
+  xhc.Run();
+
+  // 機器が接続されているUSBポートに対し、設定を行う
+  usb::HIDMouseDriver::default_observer = MouseObserver;
+
+  for (int i = 1; i < xhc.MaxPorts(); ++i) {
+    auto port = xhc.PortAt(i);
+    Log(kDebug, "Port %d: IsConnected=%d\n", i, port.IsConnected());
+
+    if (port.IsConnected()) {
+      if (auto err = ConfigurePort(xhc, port)) {
+        Log(kError, "failed to configure port: %s at %s:%d\n",
+            err.Name(), err.File(), err.Line());
+        continue;
+      }
+    }
+  }
+
+  // マウス動作時のイベント処理(ポーリング)
+  while (1) {
+    if (auto err = ProcessEvent(xhc)) {
+      Log(kError, "Error while ProcessEvent: %s at %s:%d\n",
+          err.Name(), err.File(), err.Line());
+    }
+  }
+
   while (1) __asm__("hlt");
 }
 
+extern "C" void __cxa_pure_virtual() {
+  while (1) __asm__("hlt");
+}
