f36a5cb839435c429d7df727303968c59a1bb724
jiftechnify <jiftech.stlfy@gmail.com>
2021-05-17 23:11:01 +0900

    Add: PutString syscall

--- kernel/terminal.hpp
+++ kernel/terminal.hpp
@@ -2,6 +2,8 @@
 
 #include <memory>
 #include <queue>
+#include <map>
+#include <optional>
 #include "graphics.hpp"
 #include "window.hpp"
 #include "fat.hpp"
@@ -12,18 +14,19 @@ class Terminal {
     static const int kRows = 15, kColumns = 60;
     static const int kLineMax = 128;
 
-    Terminal();
+    Terminal(uint64_t task_id);
     unsigned int LayerID() const { return layer_id_; }
     Rectangle<int> BlinkCursor();
     Rectangle<int> InputKey(uint8_t modifier, uint8_t keycode, char ascii);
     void Print(char c);
-    void Print(const char* s);
+    void Print(const char* s, std::optional<size_t> len = std::nullopt);
     void ExecuteLine();
     Error ExecuteFile(const fat::DirectoryEntry& entry, char* command, char* first_arg);
 
   private:
     std::shared_ptr<ToplevelWindow> window_;
     unsigned int layer_id_;
+    uint64_t task_id_;
 
     Vector2D<int> cursor_{0, 0};
     bool cursor_visible_{false};
@@ -39,6 +42,9 @@ class Terminal {
     int cmd_history_index_{-1};
     Rectangle<int> HistoryUpDown(int direction);
 };
+
+// タスクIDからターミナルインスタンスを取得するためのMap
+extern std::map<uint64_t, Terminal*>* terminals;
 
 void TaskTerminal(uint64_t task_id, int64_t data);
 
